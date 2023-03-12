92617b5e1aaf4c0477ba523178c68ded405eb3b1
PoYu Han <poyuhan085@gmail.com>
2023-03-12 09:37:44 +0800

    Added login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,5 +8,6 @@ router.register('profile', views.UserProfileViewSet)  # No need to set the basen
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
